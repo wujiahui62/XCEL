980b3d3a4a7d14a6729dd3f63415fa2f77d35304
Jiahui Wu <wujiahui62@gmail.com>
2018-09-27 15:55:07 -0400

    added start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export FLASK_APP=xcel.py
+export FLASK_DEBUG=0
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=thexcelfuture@gmail.com
+export MAIL_PASSWORD=PET-mD8-NsL-MdL
